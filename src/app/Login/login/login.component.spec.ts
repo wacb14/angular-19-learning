fb32c259de6a5baefbb0e12e6ddc02c071cd3031
Willy <wacb14@gmail.com>
2025-05-22 21:50:16 -0500

    Login unitary test

--- src/app/Login/login/login.component.spec.ts
+++ src/app/Login/login/login.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 import { LoginComponent } from './login.component';
-import { firstValueFrom } from 'rxjs';
+import { render } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
+import { screen } from '@testing-library/angular'; // ✅ Agrega esta línea
+import { TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
@@ -12,21 +16,75 @@ TestBed.initTestEnvironment(
 );
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let authServiceMock: jest.Mocked<AuthService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
+    authServiceMock = {
+      login: jest.fn(),
+    } as unknown as jest.Mocked<AuthService>;
 
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    delete (window as any).location;
+    window.location = { href: '' } as any;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Should redirect to the dashboard', async () => {
+    //Given
+    authServiceMock.login.mockReturnValueOnce(of({ token: 'fake-jwt-token' }));
+
+    await render(LoginComponent, {
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
+    });
+
+    // When
+    await userEvent.type(
+      screen.getByPlaceholderText('Email'),
+      'user@example.com'
+    );
+    await userEvent.type(
+      screen.getByPlaceholderText('Password'),
+      'password1234'
+    );
+    await userEvent.click(screen.getByRole('button', { name: /login/i }));
+
+    // Then
+    expect(authServiceMock.login).toHaveBeenCalledWith(
+      'user@example.com',
+      'password1234'
+    );
+    expect(window.location.href).toBe('/dashboard');
+  });
+
+  it('Should throw an error when the login fails', async () => {
+    //Given
+    authServiceMock.login.mockReturnValueOnce(
+      throwError(() => {
+        error: {
+          message: 'Invalid email or password';
+        }
+      })
+    );
+
+    await render(LoginComponent, {
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
+    });
+
+    // When
+    await userEvent.type(
+      screen.getByPlaceholderText('Email'),
+      'user@example.com'
+    );
+    await userEvent.type(
+      screen.getByPlaceholderText('Password'),
+      'wrongPassword'
+    );
+    await userEvent.click(screen.getByRole('button', { name: /login/i }));
+
+    // Then
+    expect(authServiceMock.login).toHaveBeenCalledWith(
+      'user@example.com',
+      'wrongPassword'
+    );
+    const errorMessage = await screen.findByText('Invalid email or password');
+    expect(errorMessage).toBeTruthy();
   });
 });
--- src/app/Login/login/login.component.ts
+++ src/app/Login/login/login.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import {
+  ReactiveFormsModule,
+  NonNullableFormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+
+interface LoginForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  errorMessage: string | null = null;
+  authService = inject(AuthService);
+  fb = inject(NonNullableFormBuilder);
 
+  loginForm = this.fb.group<LoginForm>({
+    email: this.fb.control('', Validators.required),
+    password: this.fb.control('', Validators.required),
+  });
+
+  login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const { email, password } = this.loginForm.getRawValue();
+    this.authService.login(email, password).subscribe({
+      next: () => {
+        window.location.href = '/dashboard';
+      },
+      error: (_error) => {
+        this.errorMessage = 'Invalid email or password';
+      },
+    });
+  }
 }
